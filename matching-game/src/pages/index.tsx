b520f1f89f8d4612b6dd412986ce75e2ce7d8fea
brentdolan <mac@Brents-MacBook-Pro.local>
2023-06-17 10:39:25 -0500

    MG-9: Cleared Global CSS

--- matching-game/src/pages/index.tsx
+++ matching-game/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import {Card} from "@/components/card";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
